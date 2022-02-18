5e6d5e4a1d110a8a439c88ea2dd493a8e06dd6f0
Forest Gregg <fgregg@datamade.us>
2022-02-17 23:05:27 -0500

    make sure filing last updated gets updated

---
+++ scripts/filing_update.sql
@@ -0,0 +1,20 @@
+UPDATE
+    filing
+SET
+    updated_at = u.updated_at
+FROM (
+    SELECT
+        case_number,
+        MAX(filing.updated_at, COALESCE(sought_unit.created_at, ''), coalesce(docket.created_at, ''), COALESCE(participant.created _at, ''), coalesce(document.created_at, ''), COALESCE(allegation.created_at, ''), coalesce(filin g_group.updated_at, ''), COALESCE(election.created_at, ''), coalesce(voting_unit.created_at, '')) updated_at
+    FROM
+        filing
+    LEFT JOIN sought_unit USING (case_number)
+    LEFT JOIN docket USING (case_number)
+    LEFT JOIN participant USING (case_number)
+    LEFT JOIN document USING (case_number)
+    LEFT JOIN allegation USING (case_number)
+    LEFT JOIN filing_group USING (case_number)
+    LEFT JOIN election USING (case_number)
+    LEFT JOIN voting_unit USING (case_number)) AS u
+WHERE
+    filing.case_number = u.case_number;
