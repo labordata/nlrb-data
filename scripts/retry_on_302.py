3f8afd8df896b9fe1b6fffbed3998bc08e5e6fe0
Forest Gregg <fgregg@datamade.us>
2022-08-30 13:05:31 -0400

    don't check ssl

--- scripts/retry_on_302.py
+++ scripts/retry_on_302.py
@@ -6,7 +6,7 @@ url = sys.stdin.read().strip()
 outname = sys.argv[1]
 
 for _ in range(20):
-    command_string = f"wget --max-redirect=0 --retry-connrefused --tries=100 {url} -O {outname}"
+    command_string = f"wget --no-check-certificate --max-redirect=0 --retry-connrefused --tries=100 {url} -O {outname}"
     result = subprocess.run(command_string, shell=True, stderr=subprocess.PIPE)
     print(result.stderr.decode(), file=sys.stderr)
     if result.returncode == 0:
