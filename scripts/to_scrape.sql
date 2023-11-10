700121f7c62ac0324d034c680015683f470a7932
Forest Gregg <fgregg@datamade.us>
2023-11-10 15:50:13 -0500

    formatting

--- scripts/to_scrape.sql
+++ scripts/to_scrape.sql
@@ -1,27 +1,30 @@
 /*
-inspired by Cho and Molina, Estimating Frequency of Change https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=60c8e42055dfb80072a547c73fbc18dfbacc20aa
+inspired by Cho and Molina, Estimating Frequency of Change
+https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=60c8e42055dfb80072a547c73fbc18dfbacc20aa
 
-Let t_changed_i be the last time we recorded a change in a case i and let
-t_checked_i be the last time we checked if the case changed, then a reasonable
-estimator of the rate of change is r_i = 1/(t_checked_i - t_changed_i).
+Let t_changed_i be the last time we recorded a change in a case i and
+let t_checked_i be the last time we checked if the case changed, then
+a reasonable estimator of the rate of change is r_i = 1/(t_checked_i -
+t_changed_i).
 
-However, if the last time we checked for a change, the case was changed then
-t_checked_i - t_changed_i = 0, and r_i will be undefined.
+However, if the last time we checked for a change, the case was
+changed then t_checked_i - t_changed_i = 0, and r_i will be undefined.
 
-We can smooth that out by using a prior belief for the rate of change. We will
-use the average rate of change:
+We can smooth that out by using a prior belief for the rate of
+change. We will use the average rate of change:
 
 B = (sum_i 1 / (t_checked_i - t_changed_i)) / N
 
 for all cases where t_checked_i > t_changed_i
 
-We can will control the effect of the prior with the parameter A, so that the
-case specific estimate of the rate of change is
+We can will control the effect of the prior with the parameter A, so
+that the case specific estimate of the rate of change is
 
 r_i = (A + 1) / (A/B + (t_checked_i - t_changed_i))
 
-With this case specific estimate we can use the Poisson distribution to calculate
-the probability that a case has changed since the last time we checked it.
+With this case specific estimate we can use the Poisson distribution
+to calculate the probability that a case has changed since the last
+time we checked it.
 
 p_i = 1 - exp(-r_i * (t_now - t_checked_i))
 
